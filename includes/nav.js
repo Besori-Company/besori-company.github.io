2d552cb5b10afa51fd82c2cb026557fe4a78c891
Besori <besoricompany@gmail.com>
2025-09-16 19:20:11 +0200

    Update nav.js

--- includes/nav.js
+++ includes/nav.js
@@ -2,6 +2,5 @@ document.getElementById("nav-placeholder").innerHTML = `
 <nav class="barra-nav" role="navigation" aria-label="Menú principal">
     <a href="/index.html">Inicio</a>
     <a href="/nosotros.html">Nosotros</a>
-    <a href="#">Apoyo</a>
 </nav>
 `;
