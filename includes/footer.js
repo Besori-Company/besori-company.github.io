3ebb1d2895e2b3288a14dd0240177a4c6b4a5e34
Besori <besoricompany@gmail.com>
2025-09-17 19:18:24 +0200

    Add files via upload

--- includes/footer.js
+++ includes/footer.js
@@ -1,7 +1,7 @@
 document.getElementById("footer-placeholder").innerHTML = `
 <footer role="contentinfo">
     <span>© <span id="year"></span> Besori ®</span>
-    <span><a href="terms.html">Términos y Políticas</a></span>
+    <span><a href="/pages/terms.html">Términos y Políticas</a></span>
     <span><a href="mailto:besoricompany@gmail.com">besoricompany@gmail.com</a></span>
 </footer>
 `;
