dab1e2cd15fc00d825c8e4c1e66c2e5191612264
Besori <besoricompany@gmail.com>
2025-09-29 21:03:30 +0200

    Add files via upload

--- js/animacion_fondo.js
+++ js/animacion_fondo.js
@@ -1,23 +1,23 @@
+const containers = document.querySelectorAll('.producto_contenido');
 
-const containers = document.querySelectorAll('.image-container');
- containers.forEach(container => {
-   container.addEventListener('mouseenter', () => {
-     document.documentElement.classList.add('hover-bg');
-   });
-   container.addEventListener('mousemove', (e) => {
-     document.documentElement.style.setProperty('--x', `${e.clientX}px`);
-     document.documentElement.style.setProperty('--y', `${e.clientY}px`);
-     const bg = document.documentElement;
-     const before = getComputedStyle(bg, '::before');
-     bg.style.setProperty('--before-top', `${e.clientY}px`);
-     bg.style.setProperty('--before-left', `${e.clientX}px`);
-     bg.style.setProperty('--x', `${e.clientX}px`);
-     bg.style.setProperty('--y', `${e.clientY}px`);
-     bg.style.setProperty('--circle-x', `${e.clientX}px`);
-     bg.style.setProperty('--circle-y', `${e.clientY}px`);
-     bg.style.setProperty('--circle-transform', `translate(-50%, -50%)`);
-   });
-   container.addEventListener('mouseleave', () => {
-     document.documentElement.classList.remove('hover-bg');
-   });
- });
+containers.forEach(container => {
+  container.addEventListener('mouseenter', () => {
+    document.documentElement.classList.add('hover-bg');
+
+    if (container.classList.contains('kael')) {
+      document.documentElement.style.setProperty('--circle-color', 'rgba(255, 140, 0, 0.55)'); // naranja
+    } else {
+      document.documentElement.style.setProperty('--circle-color', 'rgba(97, 204, 174, 0.81)'); // Cloud
+    }
+  });
+
+  container.addEventListener('mousemove', (e) => {
+    document.documentElement.style.setProperty('--circle-x', `${e.clientX}px`);
+    document.documentElement.style.setProperty('--circle-y', `${e.clientY}px`);
+    document.documentElement.style.setProperty('--circle-transform', 'translate(-50%, -50%)');
+  });
+
+  container.addEventListener('mouseleave', () => {
+    document.documentElement.classList.remove('hover-bg');
+  });
+});
