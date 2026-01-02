f9e60c579b74e9fb5b37a01e785dbcae6cca1942
Besori-Company <besoricompany.com>
2026-01-02 15:47:33 +0100

    correo nuevo

--- includes/footer.js
+++ includes/footer.js
@@ -2,7 +2,7 @@ document.getElementById("footer-placeholder").innerHTML = `
 <footer role="contentinfo">
     <span>© <span id="year"></span> Besori ®</span>
     <span><a href="/pages/terms.html">Términos y Políticas</a></span>
-    <span><a href="mailto:besoricompany@gmail.com">besoricompany@gmail.com</a></span>
+    <span><a href="mailto:info.besori@gmail.com">info.besori@gmail.com</a></span>
 </footer>
 `;
 document.getElementById("year").textContent = new Date().getFullYear();
